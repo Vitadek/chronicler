25871e2ee3b5dbcc6974ca74ca73bfd45c1fb3ac
Vitadek <author@example.com>
2026-07-18 07:38:21 -1000

    build: Rebuild embedded assets with settings page white-screen bugfix

--- web/assets/index-DX_FxDtw.js
+++
@@ -1,6 +0,0 @@
-import{bg as s,dM as i,bT as f}from"./index-D6Fgqcn0.js";function g(r,c){for(var o=0;o<c.length;o++){const e=c[o];if(typeof e!="string"&&!Array.isArray(e)){for(const t in e)if(t!=="default"&&!(t in r)){const a=Object.getOwnPropertyDescriptor(e,t);a&&Object.defineProperty(r,t,a.get?a:{enumerable:!0,get:()=>e[t]})}}}return Object.freeze(Object.defineProperty(r,Symbol.toStringTag,{value:"Module"}))}/**
- * @license lucide-react v0.546.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const p=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],l=s("chevron-right",p);var n=i();const u=f(n),m=g({__proto__:null,default:u},[n]);export{l as C,m as R,u as a,n as r};
