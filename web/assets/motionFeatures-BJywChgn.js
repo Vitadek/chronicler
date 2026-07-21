12d32da2fb7abeeed3a76d92305147e24f6b3fd6
Vitadek <author@example.com>
2026-07-20 17:19:30 -1000

    build: Track embedded web/ assets for Flatpak builds

---
+++ web/assets/motionFeatures-BJywChgn.js
@@ -0,0 +1,1 @@
+import{a}from"./features-max-Bk9Gaa3q.js";import"./layout-Dq8ubFjp.js";import"./index-CdmnVL5S.js";export{a as default};
---
+++ web/assets/proxy-DzlZ2yav.js
@@ -0,0 +1,1 @@
+import{p as a,aT as o,ah as t,ao as s,a7 as e}from"./layout-Dq8ubFjp.js";import{ah as r}from"./index-CdmnVL5S.js";const m={...a,...s,...t,...o},u=r(m,e);export{u as m};
